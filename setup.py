b149485d6aa8867e34c133076cc9937a532cc6b5
rodrigoneal <rodrigho2006@gmail.com>
2021-02-04 14:54:07 -0300

    corrigindo erros

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="desafio-storm",
+    name="desafios-storm",
     version="0.1.0",
     description="Desafio da empresa storm para a vaga de backend",
-    packagens=find_packages(),
+    packages=find_packages(),
     include_package_data=True,
-    install_requires=[],
+    install_requires=["wheel"],
 )
